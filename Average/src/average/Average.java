70ddf2ad700c151601e5b0179eb837c78055dc42
pratikgholam <gholampratik2@gmail.com>
2017-05-08 17:00:01 +0530

    commiting

---
+++ Average/src/average/Average.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package average;
+
+/**
+ *
+ * @author MRuser
+ */
+public class Average {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int average,a,b,c;
+        a=10;
+        b=4;
+        c=25;
+        average=a+b+c/3;
+        System.out.println(average);
+    }
+    
+}
---
+++ HelloApp/src/com/example/helloapp/ButtonListener.java
@@ -0,0 +1,14 @@
+package com.example.helloapp;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class ButtonListener implements OnClickListener {
+
+	@Override
+	public void onClick(View arg0) {
+		System.out.println("Button Clicked");
+
+	}
+
+}
